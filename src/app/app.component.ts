2133bdd62edbb8ff32ce50a556d71725dfa77416
Olexander Podik <podikalexander@gmail.com>
2024-07-17 02:51:12 +0300

    adjust text color and alignment in dialogue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ import { Playable } from './models/playable/playable.model';
 })
 export class AppComponent {
   title = 'Intervality';
-  version = '0.14.1';
+  version = '0.14.2';
   isPanelVisible = false;
   isPushMode = !(window.innerWidth < 640);
   isPlayer = signal(false);
