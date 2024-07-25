4d8b056c357f04050736501a95e97a7afa0464f2
Olexander Podik <podikalexander@gmail.com>
2024-07-25 18:54:58 +0300

    fix reports dont show up in the history list
    remove date pipe that caused the issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ import { BeepService } from './player/beep.service';
 })
 export class AppComponent {
   title = 'Intervality';
-  version = '0.16.1';
+  version = '0.16.2';
   isPanelVisible = false;
   isPushMode = !(window.innerWidth < 640);
   isPlayer = signal(false);
